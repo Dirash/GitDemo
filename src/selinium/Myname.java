600d29c796b312bb52978263f963285fb03c1ae5
Dirash2 <dirashche10+1@gmail.com>
2024-04-11 00:54:30 +0530

    develop branch 1

--- src/selinium/Myname.java
+++ src/selinium/Myname.java
@@ -3,6 +3,16 @@ package selinium;
 public class Myname {
 	public void Totaol(){
 		System.out.println("new");
+		System.out.println("new");
+		System.out.println("new");
+		System.out.println("new");
 	}
+	public void Totaol2(){
+		System.out.println("old");
+		System.out.println("old");
+		System.out.println("old");
+		System.out.println("old");
+	}
+
 
 }
